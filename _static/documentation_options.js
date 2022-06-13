696290d3326e9883f2c0458348e0b970db1cb3aa
lfarv <lfarv@users.noreply.github.com>
2022-06-13 16:16:03 +0000

    deploy: cb6c1036fc2667551c1a95acc455499314cade9e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g6b4e050',
+    VERSION: '0.1.dev1+gcb6c103',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
