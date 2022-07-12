492238c16600dedb49f157631c2cd9e88b928277
lnadolski <lnadolski@users.noreply.github.com>
2022-07-12 17:54:47 +0000

    deploy: a007efc248aa435bc185a415ef8f8309095dc3d0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g07885e1',
+    VERSION: '0.1.dev1+ga007efc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
