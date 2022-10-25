ede8b4aaa4c493c8f9437f4dbeb09e292c21064e
swhite2401 <swhite2401@users.noreply.github.com>
2022-10-25 08:44:02 +0000

    deploy: 8e08f2a28ea33bce5bc9a84284c105bea6e985c2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gb2055db',
+    VERSION: '0.1.dev1+g8e08f2a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
